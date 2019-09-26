7b6f8a128a46b09594c95ffd22ae872ce057dd7c
Allan <allanjegu@gmail.com>
2019-09-26 21:12:39 +0200

    Nettoyage du template

---
+++ src/components/LogOut/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-button @click="logout">Déconnexion</el-button>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout: () => {
+      console.log('logout ')
+    }
+  }
+}
+</script>
--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -5,28 +5,7 @@
     <breadcrumb class="breadcrumb-container" />
 
     <div class="right-menu">
-      <el-dropdown class="avatar-container" trigger="click">
-        <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
-          <i class="el-icon-caret-bottom" />
-        </div>
-        <el-dropdown-menu slot="dropdown" class="user-dropdown">
-          <router-link to="/">
-            <el-dropdown-item>
-              Home
-            </el-dropdown-item>
-          </router-link>
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
-            <el-dropdown-item>Github</el-dropdown-item>
-          </a>
-          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
-            <el-dropdown-item>Docs</el-dropdown-item>
-          </a>
-          <el-dropdown-item divided>
-            <span style="display:block;" @click="logout">Log Out</span>
-          </el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
+      <log-out />
     </div>
   </div>
 </template>
@@ -35,11 +14,13 @@
 import { mapGetters } from 'vuex'
 import Breadcrumb from '@/components/Breadcrumb'
 import Hamburger from '@/components/Hamburger'
+import LogOut from '@/components/LogOut'
 
 export default {
   components: {
     Breadcrumb,
-    Hamburger
+    Hamburger,
+    LogOut
   },
   computed: {
     ...mapGetters([
